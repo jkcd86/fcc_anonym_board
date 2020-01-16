1b47f2550ed738afb3309b90505cb0ac5312fd51
jkcd <jkcd@np900.IKBnet>
2020-01-16 14:41:13 +0100

    Initial commit

---
+++ tests/1_unit-tests.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var assert = chai.assert;
+//var ConvertHandler = require('../controllers/convertHandler.js');
+
+//var convertHandler = new ConvertHandler();
+
+suite('Unit Tests', function () {
+
+});
